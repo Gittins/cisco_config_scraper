cc8ff8bcb167d67adb03bc5928a523f88fe66012
Anthony Gittins <anthonygittins@gmail.com>
2016-10-26 12:13:39 +0000

    version 1 file updated

--- cisco_cs_01.py
+++ cisco_cs_01.py
@@ -5,7 +5,27 @@
 
 
 
-import ciscoconfparse
+from ciscoconfparse import CiscoConfParse
 
+configFileName = raw_input("Please provide the name of the config file in the files folder: ")
+objectListFileName = raw_input("Please provide the name of the object list file in the files folder: ")
+folderPath = '/vagrant/python-apps/cisco_config_scraper/cisco_config_scraper/scripts/files/'
 
+configFile = open(folderPath + configFileName + ".txt", 'r')
+objectListFile = open(folderPath + objectListFileName + ".txt", 'r') 
 
+#print str(configFile.read())
+#print str(objectListFile.read())
+
+def objctLookup():
+    isIn = " is in config"
+    isNotIn = "is not in config"
+    for object in objectListFile:
+        print object
+        print ""
+        for line in configFile:
+            if object in line:
+               return object + isIn
+        return object + isNotIn
+
+print objctLookup
